9205c58e7d29147b86adfde1b56f565432a9454b
Jayakrishnan K V <jayakrishnankv1994@gmail.com>
2024-12-04 17:05:03 +0100

    added health check

--- app/main.py
+++ app/main.py
@@ -2,17 +2,16 @@ from fastapi import FastAPI
 from .models import Base
 from .data_base import engine
 from .routers import auth, predict, users, admin
-import uvicorn
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+@app.get("/healthy")
+def health_check():
+    return {'status': 'Healthy'}
+
 app.include_router(auth.router)
 app.include_router(predict.router)
 app.include_router(admin.router)
 app.include_router(users.router)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
